4d1da3491a178fda8f06f4cb7881faf97fcd55a6
spofabsss <spofabs@gmail.com>
2026-07-09 20:47:58 -0600

    Update clase08_analisis_hospital.py

--- Clase8/clase08_analisis_hospital.py
+++ Clase8/clase08_analisis_hospital.py
@@ -44,7 +44,11 @@ else:
 
     # Variables acumuladoras del analisis.  suma_edades, conteo_san_jose, 
     # conteo_mujeres, conteo_hombres y adultos_mayores.
-
+    suma_edades = 0
+    conteo_san_jose= 0
+    conteo_mujeres =0
+    conteo_hombres =0
+    adultos_mayores= []
 
     # 4. Ciclo principal
     # Cada vuelta del ciclo representa un paciente del JSON.
@@ -58,16 +62,22 @@ else:
         # Complete aqui los acumuladores dentro del ciclo.
 
         # 3.1 Sume la edad del paciente en suma_edades
-
+        suma_edades +=edad
         # 3.2 Si la provincia es "San Jose", aumente conteo_san_jose
-
+        if provincia=="San Jose":
+            conteo_san_jose +=1
         # 3.3 Si genero es "F", aumente conteo_mujeres
-
+        if genero == "F":
+            conteo_mujeres +=1
         # 3.4 Si genero es "M", aumente conteo_hombres
-
+        if genero == "M":
+            conteo_hombres +=1
         # 3.5 Si es_adulto_mayor(edad) es True, agregue el nombre
+        if es_adulto_mayor (edad):
+            adultos_mayores.append(nombre)
+            
         # a adultos_mayores
-
+       
         # RETO FINAL OPCIONAL:
         # Cada paciente tiene una lista en paciente["enfermedades"].
         # Guarde esa lista en una variable y sume su cantidad con len().
@@ -75,17 +85,18 @@ else:
     # REQUERIMIENTO 4:
     # Calcule la edad_promedio usando calcular_promedio().
     edad_promedio = 0
+    promedio = calcular_promedio(suma_edades,len(pacientes))
 
     # Resultados
-    #print("\nRESUMEN BASICO")
-    #print("Edad promedio:", round(edad_promedio, 1))
-    #print("Pacientes de San Jose:", conteo_san_jose)
-    #print("Mujeres:", conteo_mujeres)
-    #print("Hombres:", conteo_hombres)
-    #print("Adultos mayores:", adultos_mayores)
+    promedio = calcular_promedio(suma_edades, len(pacientes))
 
+    print("Edad promedio:", promedio)
+    print("Pacientes de San José:", conteo_san_jose)
+    print("Mujeres:", conteo_mujeres)
+    print("Hombres:", conteo_hombres)
+    print("Adultos mayores:", adultos_mayores)
     # REQUERIMIENTO 5:
     # Escriba dos conclusiones basadas en los resultados.
-    print("\nCONCLUSIONES")
-    print("Conclusion 1: ______________________________")
-    print("Conclusion 2: ______________________________")
+    print("CONCLUSIONES:")
+    print("1. La edad promedio de los pacientes es de", promedio, "años.")
+    print("2. Hay", conteo_san_jose, "pacientes provenientes de San Jose.")
