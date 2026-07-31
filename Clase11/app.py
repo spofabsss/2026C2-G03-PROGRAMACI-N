912a64400410e4af27de6f834371739b86c4c0ec
spofabsss <spofabs@gmail.com>
2026-07-30 21:04:25 -0600

    clase11

--- Clase11/app.py
+++ Clase11/app.py
@@ -1,17 +1,55 @@
 """Programa principal del proyecto modular BCCR."""
-print ("Inicio el programa")
-from lectura_datos import cargar_tabla_bccr 
+
+from lectura_datos import cargar_tabla_bccr, mostrar_top_10 
 from limpieza_datos import limpiar_datos
 
 def ejecutar (): 
     """Cargar los datos y presentar el menu del sistema"""
     datos_crudos = cargar_tabla_bccr()
     datos= limpiar_datos (datos_crudos)
-    
     print (datos.head())
-    while True:
-        print ("\nPROYECTO DE ANALISIS BCCR")
-        print ("1. Mostrar primeras 10 entidades limpias")
+
+if __name__ == "__main__":
+    ejecutar()
     
-    if __name__ == "__main__":
-        ejecutar ()
+    while True: 
+        print("\nPROYECTO DE ÁNALISIS BCCR")
+        print("1. Mostrar primeras 10 entidades limpias.")
+        print("2. Promedio por tipo entidad.")
+        print("3. Mostrar entidades financieras con diferencial mayor al promedio.")
+        print("4. Mostrar lista entidades y exportar CSV.")
+        print("5. Graficar")
+        print("6. Salir")
+        
+        opcion = input("Seleccione un opción: ").strip()
+        if opcion == "1":
+            seleccion = ["ENTIDAD", "COMPRA", "VENTA", "DIFERENCIAL"]
+            
+            print(mostrar_top_10(datos))
+        elif opcion == "2":
+            promedio_diferencial = datos["DIFERENCIAL"].mean()
+            print(f"El promedio del diferencial es: {promedio_diferencial:.2f}")
+            #Agrupa por TIPO, calcula el promedio de COMPRA, VENTA Y DIFERENCIAL, redondee y 
+            
+            columnas= ["COMPRA", "VENTA ", "DIFERENCIAL"]
+        print(
+            datos.groupby ("TIPO")[columnas]
+                .mean()
+                .round(2)
+                .sort_values("DIFERENCIAL", ascending=False)
+        )
+        
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            print("Graficando ...")
+        elif opcion == "6":
+            input("Análisis finalizado. Presione enter para salir ...")
+            break
+        else:
+            print("Opción inválida. Escriba un número del 1 al 6.")
+        input("\nPresione enter para continuar...\n")
+if __name__ == "__main__":
+    ejecutar()
